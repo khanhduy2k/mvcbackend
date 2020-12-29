34e3e6147d2eaf1126ee2841a42d9bebac16c57c
Duykhanh <khanhvip1412@gmail.com>
2020-12-30 03:43:20 +0700

    HeHe bot

--- index.js
+++ index.js
@@ -98,5 +98,5 @@ app.use(express.json());
 
 //apply routes
 route(app);
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8800;
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
