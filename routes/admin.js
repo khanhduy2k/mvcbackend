9405e350af4c68df4b8538c1f6bf16a3c80d0908
duykhanh <zerok49tt@gmail.com>
2021-03-04 07:23:17 +0700

    edit css file admin.hbs and thongbao.hbs

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,7 @@ const multer  = require('multer');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, './public/img')
+      cb(null, './public/img') 
     },
     filename: function (req, file, cb) {
       cb(null, file.originalname)
