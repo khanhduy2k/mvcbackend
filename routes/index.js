00dd4ffd14d0358e47385520ca2994950928e10f
duykhanh <zerok49tt@gmail.com>
2021-04-24 22:47:37 +0700

    loading page

--- routes/index.js
+++ routes/index.js
@@ -4,32 +4,27 @@ const profileRouter = require('./profile')
 const adminRouter = require('./admin');
 const authMiddlewares = require('../middlewares/authmiddlewares');
 
-const middleWares = [
-    authMiddlewares.requireUser,
-    authMiddlewares.requireAdminLogin,
-    authMiddlewares.requireUserlogin,
-]
 
 function route(app){
+    app.use('/', 
+        authMiddlewares.requireUser,
+        authMiddlewares.requireAdminLogin,
+        authMiddlewares.requireUserlogin,
+    siteRouter);
+
     app.use('/admin',  
-        middleWares,
         authMiddlewares.requireAdmin,
         authMiddlewares.requireAuth, 
     adminRouter);
 
     app.use('/profile',
-        middleWares,
         authMiddlewares.requireAuth, 
     profileRouter);
 
     app.use('/courses',
-        middleWares,
         authMiddlewares.requireAuth, 
     coursesRouter);
 
-    app.use('/', 
-        middleWares,
-    siteRouter);
 }
 
 module.exports = route;
